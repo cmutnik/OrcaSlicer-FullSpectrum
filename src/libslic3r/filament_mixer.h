5cc7408c300dc64bdcd795d68e711b3ea34c91b7
Corey Mutnik <cmutnik@hawaii.edu>
2026-04-18 14:48:19 -0400

    Added kubelka_munk_lerp declaration.

--- src/libslic3r/filament_mixer.h
+++ src/libslic3r/filament_mixer.h
@@ -18,6 +18,12 @@ void filament_mixer_lerp_linear_float(float r1, float g1, float b1,
                                       float t,
                                       float* out_r, float* out_g, float* out_b);
 
+// Kubelka-Munk physics-based pigment mixing (subtractive, single pass).
+void kubelka_munk_lerp(unsigned char r1, unsigned char g1, unsigned char b1,
+                       unsigned char r2, unsigned char g2, unsigned char b2,
+                       float t,
+                       unsigned char* out_r, unsigned char* out_g, unsigned char* out_b);
+
 } // namespace Slic3r
 
 #endif
